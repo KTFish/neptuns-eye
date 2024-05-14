0d62c3f86204bb3e4cd95421955327aabfab5b24
Perunio <divodar@protonmail.com>
2024-05-14 13:44:45 +0200

    fix preprocess functions for all las file formats

--- AI/preprocess/preprocess.py
+++ AI/preprocess/preprocess.py
@@ -3,24 +3,35 @@ from sklearn.model_selection import train_test_split
 
 def prepare_data_training(df, target_column="classification", test_size=0.2, random_state=42):
     """
-        Split data into training and test sets by removing specified columns and using specified target.
+        Split data into training and test sets by keeping specified columns and using specified target.
     """
-    columns_to_delete = ['synthetic', 'key_point', 'withheld', 'user_data', 'point_source_id']
-    df = df.drop(columns=columns_to_delete)
+    columns_to_keep = ["X", "Y", "Z", "intensity", "return_number", "number_of_returns",
+                       "scan_direction_flag", "edge_of_flight_line", "scan_angle_rank",
+                       "red", "green", "blue", target_column]
 
+    # Usuwamy wszystkie kolumny, które nie są w liście columns_to_keep
+    df = df[columns_to_keep]
+
+    # Rozdzielamy kolumny na zmienne niezależne i zmienną docelową
     x = df.drop(columns=[target_column])
     y = df[target_column]
 
+    # Dzielimy dane na zestawy treningowe i testowe
     return train_test_split(x, y, test_size=test_size, random_state=random_state)
 
 
 def prepare_data_prediction(df, target_column="classification"):
     """
-        Prepare prediction data by removing specified columns and separating features from the target.
+        Prepare prediction data by keeping specified columns and separating features from the target.
     """
-    columns_to_delete = ['synthetic', 'key_point', 'withheld', 'user_data', 'point_source_id']
-    df = df.drop(columns=columns_to_delete)
+    columns_to_keep = ["X", "Y", "Z", "intensity", "return_number", "number_of_returns",
+                       "scan_direction_flag", "edge_of_flight_line", "scan_angle_rank",
+                       "red", "green", "blue", target_column]
 
+    # Usuwamy wszystkie kolumny, które nie są w liście columns_to_keep
+    df = df[columns_to_keep]
+
+    # Rozdzielamy kolumny na zmienne niezależne i zmienną docelową
     test_features = df.drop(columns=[target_column])
     test_labels = df[target_column]
 
--- neptunseye/classification_utils.py
+++ neptunseye/classification_utils.py
@@ -4,31 +4,20 @@ import joblib
 class ClassificationUtils:
 
     @staticmethod
-    def prepare_data_prediction(df, target_column="classification", random_state=42):
+    def prepare_data_prediction(df, target_column="classification"):
         """
-        Prepare data for making predictions using a trained machine learning model by performing the following steps:
-        1. Drop specified columns from the DataFrame.
-        2. Sample the DataFrame with a specified sampling rate.
-        3. Separate the sampled data into features and target variable.
-
-        Parameters:
-        - df (DataFrame): The input DataFrame containing the dataset.
-        - target_column (str): The name of the target column in the DataFrame. Default is "classification".
-        - sample_rate (float): The fraction of samples to include in the sampled DataFrame.
-        Should be between 0 and 1. Default is 0.1.
-        - random_state (int): Controls the randomness of the sampling operation. Default is 42.
-
-        Returns:
-        - Tuple: A tuple containing two elements - test_features and test_labels, where:
-            - test_features (DataFrame): The features of the data to be used for prediction.
-            - test_labels (Series): The corresponding target variable values for the prediction dataset.
+            Prepare prediction data by keeping specified columns and separating features from the target.
         """
-        columns_to_delete = ['synthetic', 'key_point', 'withheld', 'user_data', 'point_source_id']
-        df = df.drop(columns=columns_to_delete)
-        df_sampled = df
+        columns_to_keep = ["X", "Y", "Z", "intensity", "return_number", "number_of_returns",
+                           "scan_direction_flag", "edge_of_flight_line", "scan_angle_rank",
+                           "red", "green", "blue", target_column]
 
-        test_features = df_sampled.drop(columns=[target_column])
-        test_labels = df_sampled[target_column]
+        # Usuwamy wszystkie kolumny, które nie są w liście columns_to_keep
+        df = df[columns_to_keep]
+
+        # Rozdzielamy kolumny na zmienne niezależne i zmienną docelową
+        test_features = df.drop(columns=[target_column])
+        test_labels = df[target_column]
 
         return test_features, test_labels
 
